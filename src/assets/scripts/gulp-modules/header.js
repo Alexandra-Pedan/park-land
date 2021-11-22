945e17f8be286f69f7171da54f057245b027a159
unknown <sashka28465@gmail.com>
2021-11-22 16:14:57 +0200

    news

--- src/assets/scripts/gulp-modules/header.js
+++ src/assets/scripts/gulp-modules/header.js
@@ -6,18 +6,18 @@
 // const header = document.querySelector('.header-js');
 // console.log(header);
 
-function handleVisibilityOnScroll(elems = [], direction = 'up') {
-  elems.forEach(elem => {
-    switch (direction) {
-      case 'down':
-        elem[0].classList.add(elem[1]);
-        break;
-      default:
-        elem[0].classList.remove(elem[1]);
-        break;
-    }
-  });
-}
+// function handleVisibilityOnScroll(elems = [], direction = 'up') {
+//   elems.forEach(elem => {
+//     switch (direction) {
+//       case 'down':
+//         elem[0].classList.add(elem[1]);
+//         break;
+//       default:
+//         elem[0].classList.remove(elem[1]);
+//         break;
+//     }
+//   });
+// }
 // locoScroll.on('scroll', position => {
 //   if (position.scroll.y > 50) {
 //     handleVisibilityOnScroll([[header, 'not-on-top']], 'down');
